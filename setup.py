cd336239e4d5709be14ff6004a057802e9dffbd2
Stiubhart Deans <stiubhart@btinternet.com>
2019-09-09 13:49:34 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='domain_config',  
+     version='0.1',
+     scripts=['domain_config'] ,
+     author="Stiubhart Deans",
+     author_email="stiubhart@btinternet.com",
+     description="Quickly configure a domain name .vhost file with an SSL Certificate using Let's Encrypt",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/stiubhart/domain_config",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
